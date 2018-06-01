fe7c1c56cbc08c91e1000cb5a83d128aef70b8e3
Guido Faecke <guido_faecke@swifttrans.com>
2018-06-01 10:38:42 -0700

    added header back in

--- src/Platform/DB2IBMiPlatform.php
+++ src/Platform/DB2IBMiPlatform.php
@@ -1,9 +1,34 @@
 <?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license. For more information, see
+ * <http://www.doctrine-project.org>.
+ */
 
 namespace DoctrineDbalIbmi\Platform;
 
 use Doctrine\DBAL\Platforms\DB2Platform;
 
+/**
+ * IBMi Db2 Schema Manager.
+ * More documentation about iSeries schema at https://www-01.ibm.com/support/knowledgecenter/ssw_ibm_i_72/db2/rbafzcatsqlcolumns.htm
+ *
+ * @author Cassiano Vailati <c.vailati@esconsulting.it>
+ * @author James Titcumb <james@asgrim.com>
+ * @author Guido Faecke <guido@emcent.com>
+ */
 class DB2IBMiPlatform extends DB2Platform
 {
     /**
